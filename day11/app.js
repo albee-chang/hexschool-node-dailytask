0f21799f118c9b5e9cc2065f244ac0383e640a9a
albee <vivic5970@gmail.com>
2023-04-24 21:44:08 +0800

    feat: 🎸 新增 Day 11

--- day11/app.js
+++ day11/app.js
@@ -5,8 +5,10 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
 //設定當接收到 GET request 時，回傳 'Hello World!' 文字
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 //執行node app.js，並造訪 http://localhost:3000/，若設定成功則會看到頁面出現 Hello World!
